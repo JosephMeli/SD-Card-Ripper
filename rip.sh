e80132cdca5a0d44913564cec810f5cb15783881
Joseph Meli <joemeli631@gmail.com>
2016-10-21 19:33:18 -0400

    Update rip.sh

--- rip.sh
+++ rip.sh
@@ -5,7 +5,6 @@
 #           written by Joseph Meli               #
 #             October 21, 2016                   #
 #                                                #
-<<<<<<< HEAD
 #              SD Ripper Script                  #
 #************************************************#
 
@@ -29,7 +28,6 @@ Mount(){
   echo "Mounting Completed"
 
 }
-
 copy(){
  for each in "$mntpoint"*; do
      echo "COPYING -----> $each"\r;
@@ -58,41 +56,12 @@ for each in "$mntpoint"*; do
     fi
   done
 }
-
-
-#check if second command line argument is null
-=======
-#       Main Driver for SD ripper functions      #
-#************************************************#
-
-#-----------------------------------------------------------------#
-# Usage:                                                           #
-# sh rip.sh [option]                                              #
-# options:                                                        #
-# -m                                                              #
-#   :calls mount function from mount_copy.sh                      #
-# -c                                                              #
-#   :calls copy function from mount_copy.sh                       #
-# -mc or -cm                                                      #
-#   :calls mount  and copy functions from mount_copy.sh           #
-# -u                                                              #
-#   :calls unmount function from unmount_clean.sh                 #
-# -x                                                              #
-#   : calls the clean function from unmount_clean.sh              #
-# -ux or -xu                                                      #
-#   : calls the unmount and clean fucntion from unmount_clean.sh  #
-#-----------------------------------------------------------------#
-
-FLAGS="$1"
-PATH="$2"
 #check if second cammad line argument is null
->>>>>>> d6fef9710a31bac603af9d250c859410016a4627
 if [[ "$PATH" = "" -o "$PATH" = " " ]]; then
   echo "need an absolute file path for secon command line argument"
   exit 1
 elif [[ "$FLAGS" = "" -o "$FLAGS" = " " ]]; then
   echo DEFAULT: Mount and Copy
-<<<<<<< HEAD
   amIRoot; Mount; copy;
 #Mount
 elif [[ "$FLAGS" = "-m" ]]; then
@@ -118,33 +87,6 @@ elif [[ "$FLAGS" = "-ux" -o "$FLAGS" = "-xu" ]]; then
 elif [[ "$FLAGS" = "-x" ]]; then
     echo Clean
     clean;
-=======
-  sh mount_copy.sh -mc "~/";
-#Mount
-elif [[ "$FLAGS" = "-m" ]]; then
-  echo Mount
-  sh mount_copy.sh -m "$PATH" ;
-#Mount and Copy
-elif [[ "$FLAGS" = "-mc" -o "$FLAGS" = "-cm" ]]; then
-  echo Mount and Copy
-  sh mount_copy.sh -mc "$PATH";
-#Copy
-elif [[ "$FLAGS" = "-c" ]]; then
-  echo Copy
-  sh mount_copy.sh -c "$PATH";
-# Unmount
-elif [[ "$FLAGS" = "-u" ]]; then
-    echo Unmount
-    sh unmount_clean.sh -u
-# Unmount and Clean
-elif [[ "$FLAGS" = "-ux" -o "$FLAGS" = "-xu" ]]; then
-    echo Unmount and Clean
-    sh unmount_clean.sh -ux
-# Clean
-elif [[ "$FLAGS" = "-x" ]]; then
-    echo Clean
-    sh unmount_clean.sh -x
->>>>>>> d6fef9710a31bac603af9d250c859410016a4627
 #help or man
 elif [[ "$FLAGS" = "-h" ]]; then
     cat help.txt
