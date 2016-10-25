eac4a5435df43fe90c1a61cab798fe8eaa96c2e6
JosephMeli <Joemeli51@hotmail.com>
2016-10-25 13:35:56 -0400

    fixed condontial expressions and runtime errors

--- rip.sh
+++ rip.sh
@@ -40,14 +40,15 @@ unmount(){
 }
 # Reads in checks yes or no with the user
 asksure() {
-echo -n "Are you sure (Y/N)? "
-while read -r -n 1 -s answer; do
-  if [[ $answer = [YyNn] ]]; then
-    [[ $answer = [Yy] ]] && retval=0
-    [[ $answer = [Nn] ]] && retval=1
-    break
-  fi
-done
+  echo -n "Are you sure (Y/N)? "
+  while read -r -n 1 -s answer; do
+    if [[ $answer = [YyNn] ]]; then
+      [[ $answer = [Yy] ]] && retval=0
+      [[ $answer = [Nn] ]] && retval=1
+      break
+    fi
+  done
+}
 # will ask user if they are sure then want to delete before rm is called
 clean(){
 if [[ asksure ]] ; then
@@ -61,44 +62,44 @@ else
 fi
 }
 #check if second cammad line argument is null
-if [[ "$PATH" = "" -o "$PATH" = " " ]]; then
+if [ "$PATH" == "" ] || [ "$PATH" == " " ]; then
   echo "need an absolute file path for secon command line argument"
   exit 1
-elif [[ "$FLAGS" = "" -o "$FLAGS" = " " ]]; then
+elif [ "$FLAGS" == "" ] || [ "$FLAGS" == " " ]; then
   echo DEFAULT: Mount and Copy
   amIRoot; Mount; copy;
 #Mount
-elif [[ "$FLAGS" = "-m" ]]; then
+elif [[ "$FLAGS" == "-m" ]]; then
   echo Mount
   amIRoot; Mount;
 #Mount and Copy
-elif [[ "$FLAGS" = "-mc" -o "$FLAGS" = "-cm" ]]; then
+elif [ "$FLAGS" == "-mc" ] || [ "$FLAGS" == "-cm" ]; then
   echo Mount and Copy
   amIRoot; Mount; copy;
 #Copy
-elif [[ "$FLAGS" = "-c" ]]; then
+elif [[ "$FLAGS" == "-c" ]]; then
   echo Copy
   amIRoot; copy;
 # Unmount
-elif [[ "$FLAGS" = "-u" ]]; then
+elif [[ "$FLAGS" == "-u" ]]; then
     echo Unmount
     amIRoot; unmount;
 # Unmount and Clean
-elif [[ "$FLAGS" = "-ux" -o "$FLAGS" = "-xu" ]]; then
+elif [ "$FLAGS" == "-ux" ] ||  [ "$FLAGS" == "-xu" ]; then
     echo Unmount and Clean
     amIRoot; unmount; clean;
 # Clean
-elif [[ "$FLAGS" = "-x" ]]; then
+elif [[ "$FLAGS" == "-x" ]]; then
     echo Clean
     clean;
 #help or man
-elif [[ "$FLAGS" = "-h" ]]; then
+elif [[ "$FLAGS" == "-h" ]]; then
     cat help.txt
-# checks for is FLAGS is a path to a file or directory 
+# checks for is FLAGS is a path to a file or directory
 elif [  -d "$FLAGS" ] || [ -f "$FLAGS" ]; then
     "$PATH" = "$FLAGS"
     amIRoot; Mount; copy;
 else
     echo "ERROR: Rip can't process command line arguments"
+    exit 1
 fi
-exit 1
